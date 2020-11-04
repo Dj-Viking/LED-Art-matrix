3b9f07e74ba37acff2788fb8990a33270838f324
DJ Viking <anders.swedishviking@gmail.com>
2020-11-04 15:51:18 -0500

    adding idea for sketch and testing serviceworker on deployment

---
+++ client/src/components/P5Sketch/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import Sketch from 'react-p5';
+
+const Sketch = () => {
+  return (
+    <>
+    <p>sketch</p>
+    </>
+  );
+};
+
+export default Sketch;
