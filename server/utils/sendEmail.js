fb24bd926d04daa82c14b180375487430dd4b678
dj-viking <anders.swedishviking@gmail.com>
2021-12-04 19:06:53 -0500

    updating send email config

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const nodemailer = require("nodemailer");
 
-const { NODEMAILER_AUTH_EMAIL, NODEMAILER_AUTH_PASS } = process.env;
+const { NODEMAILER_AUTH_EMAIL, NODEMAILER_AUTH_PASS, OTHER_NODEMAILER_AUTH_PASS } = process.env;
 
 /**
  * 
@@ -30,7 +30,7 @@ async function sendEmail(args/*: MySendEmailOptions*/) /*: Promise<void>*/ {
     //If port 587 is blocked, port 2525 makes a good alternative.
     auth: {
       user: NODEMAILER_AUTH_EMAIL, // ethereal doesn't work anymore
-      pass: NODEMAILER_AUTH_PASS, // ethereal doesn't work anymore
+      pass: OTHER_NODEMAILER_AUTH_PASS, // ethereal doesn't work anymore
     },
   });
   // console.log("creating transporter", transporter);
@@ -43,11 +43,11 @@ async function sendEmail(args/*: MySendEmailOptions*/) /*: Promise<void>*/ {
     html: mailHtml
   });
 
-  console.log("Message sent: %s", info.messageId);
+  // console.log("Message sent: %s", info.messageId);
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
   // // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
   // // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
