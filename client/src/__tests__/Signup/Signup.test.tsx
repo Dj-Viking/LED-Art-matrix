c2d0045bfafd1b6a283916f6061afb7b538a7a45
dj-viking <anders.swedishviking@gmail.com>
2022-01-17 15:48:37 -0500

    impl window.location for signup page

--- client/src/__tests__/Signup/Signup.test.tsx
+++ client/src/__tests__/Signup/Signup.test.tsx
@@ -50,6 +50,7 @@ describe("Test rendering signup correctly", () => {
   afterEach(() => {
     cleanup();
     global.fetch = originalFetch;
+    localStorage.clear();
   });
 
   it("Render the home page and then click sign up button to go to that page", async () => {
@@ -96,6 +97,7 @@ describe("test signup functionality", () => {
   afterEach(() => {
     cleanup();
     global.fetch = originalFetch;
+    localStorage.clear();
   });
 
   it("Checks the input fields are available and can submit with a stubbed api", async () => {
@@ -170,6 +172,7 @@ describe("Tests network error message", () => {
   afterEach(() => {
     cleanup();
     global.fetch = originalFetch;
+    localStorage.clear();
   });
 
   it("Checks the input fields are available and can submit with a stubbed api", async () => {
@@ -180,11 +183,11 @@ describe("Tests network error message", () => {
       </Provider>
     );
 
-    const link = await screen.findByText(/^Sign\sUp$/g);
-
-    expect(link).toBeInTheDocument();
-
-    fireEvent.click(link);
+    const page = (await screen.findAllByRole("link", { name: "Sign Up" })).find((el) => {
+      return el.classList.contains("nav-button");
+    }) as HTMLElement;
+    expect(page).toBeInTheDocument();
+    fireEvent.click(page);
 
     const formEls = {
       username: screen.getByPlaceholderText(/my_username/g) as HTMLInputElement,
@@ -213,4 +216,96 @@ describe("Tests network error message", () => {
     });
     
   });
+});
+
+describe("need to impl window.location.assign()", () => {
+  beforeEach(() => {
+    // @ts-ignore need to redefine prop for jest
+    delete window.location;
+    // console.log("deleted location", window.location);
+    // @ts-ignore need to redefine prop for jest
+    // @ts-ignore need to redefine prop for jest
+    delete window.location;
+    // console.log("checking window location", window.location);
+    // @ts-ignore need to redefine prop for jest
+    window.location = {
+      assign: jest.fn(() => {
+        return void 0;
+      })
+    };
+    // console.log("window.location.assign should be a jest mock fn", window.location);
+    // @ts-ignore
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        status: 200,
+        json: () => Promise.resolve(SIGNUP_MOCK_RESULT),
+      })
+    );
+  });
+
+  afterEach(() => {
+    cleanup();
+    localStorage.clear();
+  });
+
+  it("checks that assign() is implemented and the app tries to redirect after successful signup", async () => {
+
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+
+    const page = (await screen.findAllByRole("link", { name: "Sign Up" })).find((el) => {
+      return el.classList.contains("nav-button");
+    }) as HTMLElement;
+    expect(page).toBeInTheDocument();
+    fireEvent.click(page);
+
+    const formEls = {
+      username: screen.getByPlaceholderText(/my_username/g) as HTMLInputElement,
+      email: screen.getByPlaceholderText(/example@email.com/g) as HTMLInputElement,
+      password: screen.getByPlaceholderText(/\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*/g) as HTMLInputElement,
+      btn: screen.getAllByRole("button", { name: "Sign Up" }).find((btn) => {
+        return btn.classList.contains("form-btn");
+      }) as HTMLElement
+    };
+
+    //check the input elements are in the document
+    expect(formEls.username).toBeInTheDocument();
+    expect(formEls.email).toBeInTheDocument();
+    expect(formEls.password).toBeInTheDocument();
+    expect(formEls.btn).toBeInTheDocument();
+
+    // type inputs to form fields and submit
+
+    user.type(formEls.username, SIGNUP_MOCK_PAYLOAD.username);
+    user.type(formEls.email, SIGNUP_MOCK_PAYLOAD.email);
+    user.type(formEls.password, SIGNUP_MOCK_PAYLOAD.password);
+    
+    user.clear(formEls.username);
+    user.clear(formEls.email);
+    user.clear(formEls.password);
+    expect(formEls.username.value).toBe("");
+    expect(formEls.email.value).toBe("");
+    expect(formEls.password.value).toBe("");
+    
+    user.type(formEls.username, SIGNUP_MOCK_PAYLOAD.username);
+    user.type(formEls.email, SIGNUP_MOCK_PAYLOAD.email);
+    user.type(formEls.password, SIGNUP_MOCK_PAYLOAD.password);
+    expect(formEls.username.value).toBe(SIGNUP_MOCK_PAYLOAD.username);
+    expect(formEls.email.value).toBe(SIGNUP_MOCK_PAYLOAD.email);
+    expect(formEls.password.value).toBe(SIGNUP_MOCK_PAYLOAD.password);
+
+    //click signup to simulate api mock
+    
+    await act(async () => {
+      formEls.btn.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    });
+
+
+    expect(window.location.assign).toHaveBeenCalledTimes(1);
+    
+  });
+
 });
