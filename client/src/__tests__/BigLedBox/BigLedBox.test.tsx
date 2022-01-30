d3c7f3989441b066cbf4df846cb9f0c0b3d958fa
dj-viking <anders.swedishviking@gmail.com>
2022-01-30 13:18:28 -0500

    adding test to the slider

--- client/src/__tests__/BigLedBox/BigLedBox.test.tsx
+++ client/src/__tests__/BigLedBox/BigLedBox.test.tsx
@@ -1,10 +1,10 @@
+/* eslint-disable testing-library/no-unnecessary-act */
 // @ts-ignore
 import React from "react";
 import App from "../../App";
 import allReducers from "../../reducers";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import user from "@testing-library/user-event";
 import { render, cleanup, screen, fireEvent } from "@testing-library/react";
 import { createMemoryHistory } from "history";
 import { Router } from "react-router-dom";
@@ -13,7 +13,8 @@ import "@jest/types";
 import "@testing-library/jest-dom";
 import "@testing-library/jest-dom/extend-expect";
 import { act } from "react-dom/test-utils";
-import { ASSERT_ANIMATION, LOGIN_MOCK_PAYLOAD_USERNAME, LOGIN_MOCK_TOKEN } from "../../utils/mocks";
+import { TestService } from "../../utils/TestServiceClass";
+import { ASSERT_ANIMATION } from "../../utils/mocks";
 
 const store = createStore(
   allReducers,
@@ -33,6 +34,9 @@ window.HTMLMediaElement.prototype.addTextTrack = () => { /* do nothing */ };
 
 
 describe("Test that the animation variation slider changes the style values", () => {
+  afterEach(() => {
+    cleanup();
+  });
 
   it("tests that slider renders", async () => {
     expect(localStorage.getItem("id_token")).toBe(null);
@@ -63,10 +67,64 @@ describe("Test that the animation variation slider changes the style values", ()
       saveDefault: screen.getByTestId("saveDefault")
     };
 
-    expect(screen.getByTestId("led0-0").classList).toHaveLength(0);
+    expect(preset_buttons.rainbowTest).toBeInTheDocument();
+    expect(preset_buttons.v2).toBeInTheDocument();
+    expect(preset_buttons.waves).toBeInTheDocument();
+    expect(preset_buttons.spiral).toBeInTheDocument();
+    expect(preset_buttons.fourSpirals).toBeInTheDocument();
+    expect(preset_buttons.dm5).toBeInTheDocument();
+    expect(preset_buttons.saveDefault).toBeInTheDocument();
 
-    const slider = screen.getByTestId("test");
-    expect(slider).toBeInTheDocument();
+    expect(screen.getByTestId("led1-1").classList).toHaveLength(1);
+
+    act(() => {
+      preset_buttons.rainbowTest.dispatchEvent(TestService.createBubbledEvent("click"));
+    });
+
+
+    //before
+    const ledStyleRef = screen.getByTestId("led-style");
+
+    const styleSplit = ledStyleRef?.textContent?.split(/(\r\n|\r|\n)/) as string[];
+    expect(styleSplit.length > 0).toBe(true);
+    const delayMatches: string[] | [] = styleSplit.map(str => {
+      if (ASSERT_ANIMATION.delayStyleRegex.test(str)) return str;
+      return void 0;
+    }).filter(item => typeof item === "string") as string[] | [];
+
+    let delayValBefore = delayMatches[0].trim();
+    expect(delayValBefore).toBe("animation-delay: 0.3003225088967971s;");
+
+    let sliderRef: HTMLInputElement;
+    sliderRef = screen.getByTestId("led-anim-variation") as HTMLInputElement;
+    expect(sliderRef).toBeInTheDocument();
+
+    act(() => {
+      fireEvent.change(sliderRef, { target: { value: "100" } });
+      sliderRef.dispatchEvent(TestService.createBubbledEvent("change"));
+    });
+
+
+    sliderRef = screen.getByTestId("led-anim-variation") as HTMLInputElement;
+    expect(sliderRef.value).toBe("100"); 
+
+    // TODO: parse the style values from the stylesheet
+    // to check if the animation-duration and/or delays are
+    // changing
+
+    const ledStyleRef2 = screen.getByTestId("led-style");
+    
+    const styleSplit2 = ledStyleRef2?.textContent?.split(/(\r\n|\r|\n)/) as string[];
+    expect(styleSplit2.length > 0).toBe(true);
+    const delayMatches2: string[] | [] = styleSplit2.map(str => {
+      if (ASSERT_ANIMATION.delayStyleRegex.test(str)) return str;
+      return void 0;
+    }).filter(item => typeof item === "string") as string[] | [];
+
+    expect(delayMatches2.length).toBe(1024);
+
+    const delayValAfter = delayMatches2[0].trim();
+    expect(delayValAfter).toBe("animation-delay: 0.2079326923076923s;");
 
   });
 
