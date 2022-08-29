ced97bf3c106fffc5eafcf574bc845825f57d5a9
dj-viking <anders.swedishviking@gmail.com>
2022-08-29 01:57:32 -0400

    add safety check and warn if env.txt file does not exist, instead use env sample file

--- run.ps1
+++ run.ps1
@@ -1,5 +1,12 @@
 $Env:MY_OS = $Env:OS
 $filepath = ".\env.txt"
-$Env:ENV_TXT = Get-Content -Path $filepath -Raw
+
+if (Test-Path -Path $filepath) {
+    $Env:ENV_TXT = Get-Content -Path $filepath -Raw
+} else {
+    Write-Host "[WARN]: env.txt was not found, using env.sample.txt instead." -ForegroundColor Yellow
+    $filepath = ".\env.sample.txt"
+    $Env:ENV_TXT = Get-Content -Path $filepath -Raw
+}
 
 node .\node_modules\concurrently\bin\concurrently.js "node tswatch.js" "node .\server\node_modules\nodemon\bin\nodemon.js -L .\server\dist\index.js" "node .\client\scripts\start.js"
