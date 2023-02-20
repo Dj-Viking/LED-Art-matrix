9ede492c83922f0ebcc7b682d0fb34b4b2948d13
dj-viking <anders.swedishviking@gmail.com>
2023-02-20 08:49:36 -0500

    updating run.ps1

--- run.ps1
+++ run.ps1
@@ -10,6 +10,17 @@ else {
     $Env:ENV_TXT = Get-Content -Path $filepath -Raw
 }
 
-Write-Host "[INFO]: running prettier and lint before start for formatting standards" -ForegroundColor Cyan
+[String]$prettierArgs = ""
+[String]$serverArgs = "node .\server\node_modules\nodemon\bin\nodemon.js -L .\server\dist\index.js"
+[String]$clientArgs = "node .\client\scripts\start.js"
+[String]$tswatchArgs = "node tswatch.js"
 
-node .\node_modules\concurrently\bin\concurrently.js "node tswatch.js" "node .\server\node_modules\nodemon\bin\nodemon.js -L .\server\dist\index.js" "node .\client\node_modules\prettier\bin-prettier --write `"./**/*.{ts,tsx}`" && node .\client\node_modules\eslint\bin\eslint.js --fix `"./**/*.{ts,tsx}`" && node .\client\scripts\start.js"
+if ($args[0] -eq "prettier") {
+    Write-Host "[INFO]: running prettier and lint before start for formatting standards" -ForegroundColor Cyan
+    $prettierArgs = "node .\client\node_modules\prettier\bin-prettier --write `"./**/*.{ts,tsx}`" && node .\client\node_modules\eslint\bin\eslint.js --fix `"./**/*.{ts,tsx}`" &&"
+} else {
+    Write-Host "[INFO]: prettier was not provided as an argument to the script - not linting code" -ForegroundColor Cyan  
+}
+
+
+node .\node_modules\concurrently\bin\concurrently.js "$tswatchArgs" "$serverArgs" "$prettierArgs $clientArgs"
