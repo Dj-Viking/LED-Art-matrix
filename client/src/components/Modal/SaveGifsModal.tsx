19916ebe0421ce641c04eeae7c59e74c6f0dcf01
dj-viking <anders.swedishviking@gmail.com>
2023-04-16 12:47:44 -0400

    refactor save gifs modal

--- client/src/components/Modal/SaveGifsModal.tsx
+++ client/src/components/Modal/SaveGifsModal.tsx
@@ -6,12 +6,8 @@ import { INewGifsModalState } from "../../types";
 import styled from "styled-components";
 import { setGifModalContext, setGifModalIsOpen } from "../../actions/gif-modal-actions";
 import { getGlobalState } from "../../reducers";
-interface SaveGifsModalProps {
-    onClose: React.MouseEventHandler<HTMLElement>;
-    context: INewGifsModalState["gifsModalContext"];
-}
 
-const ModalCloseButton = styled.button`
+const StyledModalCloseButton = styled.button`
     & {
         border: 1px solid grey;
         cursor: pointer;
@@ -34,7 +30,7 @@ const ModalCloseButton = styled.button`
     }
 `;
 
-const ModalSaveButton = styled.button`
+const StyledModalSaveButton = styled.button`
     & {
         border: 1px solid grey;
         cursor: pointer;
@@ -74,6 +70,97 @@ const saveButtonDisabledStyles: React.CSSProperties = {
     borderRadius: "10px",
 };
 
+const ModalInfo: React.FC<{ text: string; input: string }> = (props) => {
+    return (
+        <span
+            data-testid="gif-modal-list-name"
+            style={{
+                color: "black",
+                marginBottom: "1em",
+            }}
+        >
+            {props.text} {props.input}
+        </span>
+    );
+};
+
+const ModalCloseButton: React.FC<{ text: string; onClose: (event?: any) => void }> = (props) => {
+    return (
+        <StyledModalCloseButton
+            role="button"
+            type="button"
+            data-testid="gif-modal-close-button"
+            onClick={(event) => {
+                props.onClose(event);
+            }}
+        >
+            {props.text}
+        </StyledModalCloseButton>
+    );
+};
+
+const ModalSaveButton: React.FC<{ input: string; handleSubmit: (event: any) => Promise<void> }> = (
+    props
+) => {
+    return (
+        <StyledModalSaveButton
+            data-testid="gif-modal-save-button"
+            disabled={props.input.length === 0}
+            role="button"
+            type="submit"
+            style={props.input.length === 0 ? saveButtonDisabledStyles : {}}
+            onClick={(event) => (async () => await props.handleSubmit(event))()}
+        >
+            SAVE
+        </StyledModalSaveButton>
+    );
+};
+
+const ModalHeader: React.FC<{ text: string }> = (props) => {
+    return <h1 style={{ color: "black" }}>{props.text}</h1>;
+};
+
+const ModalInputField: React.FC<{ input: string; handleChange: (event?: any) => void }> = (
+    props
+) => {
+    return (
+        <input
+            autoComplete="off"
+            name="list-name"
+            data-testid="gif-modal-list-name-input"
+            style={{ color: "black" }}
+            type="text"
+            value={props.input}
+            onChange={props.handleChange}
+        />
+    );
+};
+
+const ModalLabel: React.FC<{ label: string }> = (props) => {
+    return (
+        <label style={{ color: "black" }} htmlFor="list-name">
+            {props.label}
+        </label>
+    );
+};
+
+const ErrorSpan: React.FC<{ error: string }> = (props) => {
+    return (
+        <>
+            {props.error.length && (
+                <span data-testid="add-gif-error" style={{ color: "red" }}>
+                    {props.error}
+                </span>
+            )}
+        </>
+    );
+};
+
+interface SaveGifsModalProps {
+    onClose: React.MouseEventHandler<HTMLElement>;
+    context: INewGifsModalState["gifsModalContext"];
+}
+
 const SaveGifsModal: React.FC<SaveGifsModalProps> = ({ onClose, context: { listName, gif } }) => {
     const [error, setError] = useState<string>("");
     const [input, setInput] = useState<string>(listName);
@@ -84,80 +171,43 @@ const SaveGifsModal: React.FC<SaveGifsModalProps> = ({ onClose, context: { listN
         setInput(listName);
     }, [listName, gifsModalIsOpen]);
 
-    function handleChange(event: any): void {
+    const handleChange = React.useCallback((event: any): void => {
         event.preventDefault();
         setInput(event.target.value);
-    }
-    async function handleSubmit(event: any): Promise<void | unknown> {
-        event.preventDefault();
-        try {
-            await API.createGifs(AuthService.getToken() as string, gif, input);
+    }, []);
 
-            dispatch(setGifModalIsOpen(false));
-            dispatch(setGifModalContext({ listName: listName, gif: {} as any }));
+    const handleSubmit = React.useCallback(
+        async (event: any): Promise<void> => {
+            event.preventDefault();
+            try {
+                await API.createGifs(AuthService.getToken() as string, gif, input);
 
-            return void 0;
-        } catch (error) {
-            const err = error as Error;
-            setError(`Oops! ${err.message}`);
-            return void 0;
-        }
-    }
+                dispatch(setGifModalIsOpen(false));
+                dispatch(setGifModalContext({ listName: listName, gif: {} as any }));
+
+                return void 0;
+            } catch (error) {
+                const err = error as Error;
+                setError(`Oops! ${err.message}`);
+                return void 0;
+            }
+        },
+        [dispatch, gif, input, listName]
+    );
+
     return (
         <>
             <GifSaveModalContainer data-testid="gif-save-modal">
-                <ModalCloseButton
-                    role="button"
-                    type="button"
-                    data-testid="gif-modal-close-button"
-                    onClick={(event) => {
-                        onClose(event);
-                    }}
-                >
-                    CLOSE
-                </ModalCloseButton>
-                <h1 style={{ color: "black" }}>SAVE GIFS</h1>
+                <ModalCloseButton text={"CLOSE"} onClose={onClose} />
 
-                <span
-                    data-testid="gif-modal-list-name"
-                    style={{
-                        color: "black",
-                        marginBottom: "1em",
-                    }}
-                >
-                    List Name: {input}
-                </span>
+                <ModalHeader text="SAVE GIFS" />
+                <ModalInfo text={"List Name: "} input={input} />
 
                 <form>
-                    <label style={{ color: "black" }} htmlFor="list-name">
-                        List Name:
-                    </label>
-
-                    <input
-                        autoComplete="off"
-                        name="list-name"
-                        data-testid="gif-modal-list-name-input"
-                        style={{ color: "black" }}
-                        type="text"
-                        value={input}
-                        onChange={handleChange}
-                    />
-
-                    <ModalSaveButton
-                        data-testid="gif-modal-save-button"
-                        disabled={input.length === 0}
-                        role="button"
-                        type="submit"
-                        style={input.length === 0 ? saveButtonDisabledStyles : {}}
-                        onClick={(event) => (async () => await handleSubmit(event))()}
-                    >
-                        SAVE
-                    </ModalSaveButton>
-                    {error.length ? (
-                        <span data-testid="add-gif-error" style={{ color: "red" }}>
-                            {error}
-                        </span>
-                    ) : null}
+                    <ModalLabel label="List Name: " />
+                    <ModalInputField input={input} handleChange={handleChange} />
+                    <ModalSaveButton input={input} handleSubmit={handleSubmit} />
+                    <ErrorSpan error={error} />
                 </form>
             </GifSaveModalContainer>
         </>
