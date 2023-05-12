2254383771c664d192fff8014c0513f4b724e30a
dj-viking <anders.swedishviking@gmail.com>
2023-05-12 08:10:22 -0400

    change name

--- client/src/utils/newReducer.ts
+++ client/src/utils/newReducer.ts
@@ -1,6 +1,6 @@
 import { ActionReducerMapBuilder, AnyAction } from "@reduxjs/toolkit";
 
-function isAnAction<AC extends TypedActionCreator<string>>(actionToMatch: AnyAction) {
+function actionIsMatching<AC extends TypedActionCreator<string>>(actionToMatch: AnyAction) {
     return function (action: AnyAction): action is ReturnType<AC> {
         return actionToMatch.type === action.type;
     };
@@ -11,5 +11,5 @@ export function newReducer<AC extends TypedActionCreator<string>, S>(
     action: AC,
     reducer: any
 ): void {
-    builder.addMatcher(isAnAction(action), reducer);
+    builder.addMatcher(actionIsMatching(action), reducer);
 }
