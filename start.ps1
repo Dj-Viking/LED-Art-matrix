6d374efcaad6dad3b0a9444f482838fd69930af0
dj-viking <anders.swedishviking@gmail.com>
2023-12-13 23:36:35 -0500

    put back the install script file invocation to the legacy start ps1 script

--- start.ps1
+++ start.ps1
@@ -2,6 +2,11 @@ param(
     [switch]$prettier = $false
 )
 
+# if node modules didn't exist then run install scripts
+# otherwise just start the app
+if (!(Test-Path -Path ".\node_modules")) {
+    & .\install.ps1;
+}
 
 Write-Host "[INFO]: Running Application in Development mode..." -ForegroundColor Cyan
 powershell -File run.ps1 $(& { if ($prettier) { "-prettier" } })
