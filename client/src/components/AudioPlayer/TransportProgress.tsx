1ecfc828e868e7ec0e26caa2fd514d5373e7b074
dj-viking <anders.swedishviking@gmail.com>
2023-12-19 23:24:33 -0500

    able to scrub the playback transport with mouse clicks, and display track time in a better format

--- client/src/components/AudioPlayer/TransportProgress.tsx
+++ client/src/components/AudioPlayer/TransportProgress.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import React from "react";
+import { calcPositionFromRange } from "../../utils/calcPositionFromRange";
 
 export interface TransportProgressProps {
     audioRef: React.RefObject<HTMLAudioElement>;
@@ -26,17 +27,35 @@ export const TransportProgress: React.FC<TransportProgressProps> = (props) => {
         [audioRef]
     );
 
-    function convertDurationTime(secs: number): string {
-        return (Number(secs) / 60).toString().substring(0, 5).replace(".", ":");
-    }
+    // TODO: handle drag events across progress??
+    const handleClickTransport = React.useCallback(
+        (event: React.MouseEvent<HTMLProgressElement>) => {
+            const target = event.target as HTMLProgressElement;
 
-    function convertElapsedTime(secs: number): string {
-        let newTime = secs
-            .toString()
-            .substring(secs.toString().split("").indexOf(".") - 3, 7)
-            .replace(".", ":");
+            const rect = target.getBoundingClientRect();
 
-        return newTime;
+            // rect x position is 130, and rect width is 160
+            // so mouse event client X position along the progress rect clicked will produce
+            // what current time to set the currently playing audio track
+
+            const transportPosition = calcPositionFromRange(
+                event.clientX,
+                0,
+                audioRef.current!.duration,
+                rect.x,
+                rect.x + rect.width
+            );
+
+            audioRef.current!.currentTime = transportPosition;
+        },
+        [audioRef]
+    );
+
+    function convertTime(secs: number): string {
+        const date = new Date(0);
+        date.setSeconds(secs);
+        const timeString = date.toISOString().substring(11, 19);
+        return timeString;
     }
 
     React.useEffect(() => {
@@ -58,12 +77,16 @@ export const TransportProgress: React.FC<TransportProgressProps> = (props) => {
     }, [handleTimeUpdate, audioRef]);
 
     return (
-        <div style={{ display: "flex", flexDirection: "column", marginLeft: "30px" }}>
-            <progress max={audioRef.current?.duration || 0} value={audioRef.current?.currentTime || 0} />
+        <div style={{ display: "flex", flexDirection: "column", marginLeft: "30px", width: "100%" }}>
+            <progress
+                style={{ cursor: "pointer", width: "79%" }}
+                onClick={handleClickTransport}
+                max={audioRef.current?.duration || 0}
+                value={audioRef.current?.currentTime || 0}
+            />
             {audioRef.current?.currentTime ? (
                 <span>
-                    {convertElapsedTime(audioRef.current?.currentTime)} --{" "}
-                    {convertDurationTime(audioRef.current?.duration)}
+                    {convertTime(audioRef.current?.currentTime)} -- {convertTime(audioRef.current?.duration)}
                 </span>
             ) : (
                 <span style={{ visibility: "visible" }}>00:00:00 -- 00:00:00</span>
