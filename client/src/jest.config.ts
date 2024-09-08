61aff68d5bd9101009fbbb6c66b5f460ab051552
dj-viking <anders.swedishviking@gmail.com>
2024-09-08 11:38:24 -0400

    mocking audio stuff for jest

--- client/src/jest.config.ts
+++ client/src/jest.config.ts
@@ -1,8 +1,12 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-useless-constructor */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { Config } from "@jest/types";
 import { defaults } from "jest-config";
 
 import Enzyme from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { INITIAL_GAIN } from "./constants";
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -77,10 +81,45 @@ window.indexedDB = {
         ),
 };
 
+// @ts-ignore
+const GainNodeProto: typeof GainNode.prototype = {
+    connect: () => ({} as any), 
+    gain: { value: INITIAL_GAIN } as any
+};
+
+// @ts-ignore
+const MockGainNodeClass = class { constructor() {} };
+Object.setPrototypeOf(MockGainNodeClass, GainNodeProto);
+// @ts-ignore
+window.GainNode = MockGainNodeClass;
+
+// @ts-ignore
+const AnalyserNodeProto: typeof AnalyserNode.prototype = {
+    "connect": () => ({} as any),
+    "frequencyBinCount": 1024,
+    "fftSize": 2048,
+    "getFloatFrequencyData": () => new Float32Array(1024)
+};
+
+const MockAnalyserClass = class { constructor() {} };
+Object.setPrototypeOf(MockAnalyserClass, AnalyserNodeProto);
+// @ts-ignore
+window.AnalyserNode = MockAnalyserClass;
+
+// @ts-ignore
+const AudioContextProto: typeof AudioContext.prototype = {
+    "createGain": () => new MockGainNodeClass() as any,
+    "createAnalyser": () => new MockAnalyserClass() as any
+};
+// @ts-ignore
+window.AudioContext = class { constructor() {} };
+Object.setPrototypeOf(window.AudioContext, AudioContextProto);
+
 export async function MyConfig(): Promise<Config.InitialOptions> {
     return {
         verbose: true,
         watchAll: true,
+        testEnvironment: "jsdom",
         // restoreMocks: true, // even with this, jest mocks are still leaking into other tests!!!!
         // clearMocks: true,
         // resetMocks: true,
